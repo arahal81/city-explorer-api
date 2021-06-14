5780a1b745002d61e0dced50b518b26b83383e91
Ali Rahhal <arahal81@gmail.com>
2021-06-15 01:37:46 +0300

    -

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(cors());
 //
 app.get('/', function (req, res) {res.send('weather data Backend');});
 
-app.get(`/weather`, (req, res) => {let cityName = req.query.S_Q;
+/*app.get(`/weather`, (req, res) => {let cityName = req.query.S_Q;
   if(C_array.includes(cityName.toUpperCase()))
   {const dataArray = data.data.map(item => new Forecast(item));
     console.log('hi');
     res.send(dataArray);}else{res.status(500).send('sorry, there is no data for the city you search for it');}
-});
+});*/
 class Forecast {
   constructor(item) {
     this.date = item.valid_date;
