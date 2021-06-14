9ae90c780d8676949706837e62b8dd2558fea365
Ali Rahhal <arahal81@gmail.com>
2021-06-15 02:10:15 +0300

    -

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const data = require('./data/weather.json');
-const PORT = 3010;
+const port = 3020;
 const C_array=['AMMAN','PARIS','SEATTLE'];
 app.use(cors());
 //
 app.get('/', function (req, res) {res.send('weather data Backend');});
 
-/*app.get(`/weather`, (req, res) => {let cityName = req.query.S_Q;
+app.get(`/weather`, (req, res) => {let cityName = req.query.S_Q;
   if(C_array.includes(cityName.toUpperCase()))
   {const dataArray = data.data.map(item => new Forecast(item));
-    console.log('hi');
     res.send(dataArray);}else{res.status(500).send('sorry, there is no data for the city you search for it');}
-});*/
+});
 class Forecast {
   constructor(item) {
     this.date = item.valid_date;
@@ -21,4 +20,4 @@ class Forecast {
   }
 }
 
-app.listen(PORT);
+app.listen(port);
