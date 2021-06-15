bf568122ba0b0994eff261d44c7ec074e6803039
Ali Rahhal <arahal81@gmail.com>
2021-06-15 13:28:55 +0300

    lab 07

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require("dotenv").config(); 
+const data = require('./data/weather.json');
+const PORT =  process.env.PORT||8080;
+const C_array=['AMMAN','PARIS','SEATTLE'];
+app.use(cors());
+//
+app.get('/', function (req, res) {res.send('weather data Backend');});
+
+app.get(`/weather`, (req, res) => {let cityName = req.query.S_Q;
+  if(C_array.includes(cityName.toUpperCase()))
+  {const dataArray = data.data.map(item => new Forecast(item));
+    res.send(dataArray);}else{res.status(500).send('sorry, there is no data for the city you search for it');}
+});
+class Forecast {
+  constructor(item) {
+    this.date = item.valid_date;
+    this.description = item.weather.description;
+  }
+}
+
+app.listen(PORT);
